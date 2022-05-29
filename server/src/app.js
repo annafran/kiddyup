32bf5c0fe93f30a2772406947aa689623db247cd
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-05-30 06:45:13 +1200

    install cors and body-parser

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const ParentModel = require("./models/ParentModel");
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = app;
