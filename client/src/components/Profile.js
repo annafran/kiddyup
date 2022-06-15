5036a90a9e0b7c8d1b23af6113fd3606c3bd1cc9
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-06-15 15:45:53 +1200

    update profile card styling

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,101 +1,110 @@
 import { Link } from "react-router-dom";
 import {
-    Card,
-    Image,
-    Text,
-    Badge,
-    Button,
-    Group,
-    useMantineTheme,
-    Title,
-    ThemeIcon,
-    Center,
+  Card,
+  Image,
+  Text,
+  Badge,
+  Button,
+  Group,
+  useMantineTheme,
+  Title,
+  ThemeIcon,
+  Center,
 } from "@mantine/core";
 import { MoodKid } from "tabler-icons-react";
 
 const Profile = ({ profile }) => {
-    const theme = useMantineTheme();
+  const theme = useMantineTheme();
 
-    const secondaryColor =
-        theme.colorScheme === "dark"
-            ? theme.colors.dark[1]
-            : theme.colors.gray[7];
+  const secondaryColor =
+    theme.colorScheme === "dark" ? theme.colors.dark[1] : theme.colors.gray[7];
 
-    const childIcon = (
-        <div>
-            <Center>
-                <ThemeIcon variant="filled" radius="lg" size="l" color="white">
-                    <MoodKid size={20} />
-                </ThemeIcon>
-            </Center>
-        </div>
-    );
+  const childIcon = (
+    <div>
+      <Center>
+        <ThemeIcon
+          variant="filled"
+          radius="lg"
+          size="m"
+          color="white"
+          style={{ backgroundColor: "#f35a59" }}
+        >
+          <MoodKid size={20} />
+        </ThemeIcon>
+      </Center>
+    </div>
+  );
 
-    const renderChild = (child) => {
-        if (child.years === 1) {
-            return `${child.years} year`;
-        }
-        if (child.years > 1) {
-            return `${child.years} years`;
-        } else if (child.months === 1) {
-            return `${child.months} month`;
-        }
-        return `${child.months} months`;
-    };
+  const renderChild = (child) => {
+    if (child.years === 1) {
+      return `${child.years} year`;
+    }
+    if (child.years > 1) {
+      return `${child.years} years`;
+    } else if (child.months === 1) {
+      return `${child.months} month`;
+    }
+    return `${child.months} months`;
+  };
 
-    return (
-        <>
-            <div style={{ width: 340, margin: "auto" }}>
-                <Card shadow="sm" p="lg">
-                    <Card.Section>
-                        <Image
-                            src={profile.profilePhoto}
-                            alt={profile.firstName}
-                        />
-                    </Card.Section>
+  return (
+    <>
+      <div style={{ width: 340, margin: "auto" }}>
+        <Card shadow="sm" p="lg">
+          <Card.Section>
+            <Image src={profile.profilePhoto} alt={profile.firstName} />
+          </Card.Section>
 
-                    <Group
-                        position="apart"
-                        style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
-                    >
-                        <Title order={1}>{profile.firstName}</Title>
-                        <Text weight={500}>{profile.city}</Text>
-                        <Badge color="pink" variant="light">
-                            {profile.parentStatus}
-                        </Badge>
+          <Group
+            position="apart"
+            style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
+          >
+            <Title style={{ color: "#345c72", fontSize: "1.8rem" }} order={1}>
+              {profile.firstName}
+            </Title>
+            <Text weight={500}>{profile.city}</Text>
+            <Badge
+              style={{ color: "#345c72", backgroundColor: "#d4edf4" }}
+              variant="light"
+            >
+              {profile.parentStatus}
+            </Badge>
 
-                        {profile.children.map((child) => {
-                            return (
-                                <div>
-                                    <Badge
-                                        sx={{ paddingLeft: 3 }}
-                                        size="lg"
-                                        radius="xl"
-                                        color="white"
-                                        variant="filled"
-                                        leftSection={childIcon}
-                                    >
-                                        {renderChild(child)}
-                                    </Badge>
-                                </div>
-                            );
-                        })}
-                    </Group>
+            {profile.children.map((child) => {
+              return (
+                <div>
+                  <Badge
+                    sx={{ paddingLeft: 3 }}
+                    size="lg"
+                    radius="xl"
+                    color="white"
+                    style={{ backgroundColor: "#f35a59" }}
+                    variant="filled"
+                    leftSection={childIcon}
+                  >
+                    {renderChild(child)}
+                  </Badge>
+                </div>
+              );
+            })}
+          </Group>
 
-                    <Button
-                        variant="light"
-                        color="blue"
-                        fullWidth
-                        style={{ marginTop: 14 }}
-                    >
-                        <Link to={`/profiles/${profile.id}`}>
-                            Connect with me
-                        </Link>
-                    </Button>
-                </Card>
-            </div>
-        </>
-    );
+          <Button
+            variant="light"
+            style={{ backgroundColor: "#d4edf4", marginTop: 14 }}
+            fullWidth
+          >
+            <Link
+              style={{ textDecoration: "none", color: "#345c72" }}
+              to={`/profiles/${profile.id}`}
+            >
+              Connect with me
+            </Link>
+          </Button>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
--- client/src/components/ProfileList.js
+++ client/src/components/ProfileList.js
@@ -1,32 +1,44 @@
 import { useEffect, useState } from "react";
-import { SimpleGrid } from "@mantine/core";
+import { Container, Grid } from "@mantine/core";
 import Profile from "./Profile";
 
 const ProfileList = () => {
-    const [profiles, setProfiles] = useState([]);
+  const [profiles, setProfiles] = useState([]);
 
-    useEffect(() => {
-        const getProfiles = async () => {
-            const response = await fetch("http://localhost:5002/profiles");
-            const data = await response.json();
-            setProfiles(data);
-        };
-        getProfiles();
-    }, []);
+  useEffect(() => {
+    const getProfiles = async () => {
+      const response = await fetch("http://localhost:5002/profiles");
+      const data = await response.json();
+      setProfiles(data);
+    };
+    getProfiles();
+  }, []);
 
-    return (
-        <div>
-            <SimpleGrid cols={5}>
-                {profiles.map((profile) => {
-                    return (
-                        <>
-                            <Profile key={profile.id} profile={profile} />
-                        </>
-                    );
-                })}
-            </SimpleGrid>
-        </div>
-    );
+  return (
+    <>
+      <Container size="80%">
+        <Grid
+          gutter="xl"
+          columns={1}
+          //   spacing={48}
+          //   breakpoints={[
+          //     { minWidth: "sm", cols: 2 },
+          //     { minWidth: "lg", cols: 3 },
+          //   ]}
+        >
+          {profiles.map((profile) => {
+            return (
+              <>
+                <Grid.Col style={{ maxWidth: 340 }}>
+                  <Profile key={profile.id} profile={profile} />
+                </Grid.Col>
+              </>
+            );
+          })}
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
 export default ProfileList;
