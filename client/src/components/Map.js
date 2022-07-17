8c065748f0df6a494366fe05841d8ad46abc4f2d
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-07-17 21:46:48 +1200

    add MarkerList component

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,22 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
-
-// import { useEffect } from "react";
 import Marker from "./Marker";
+
+const MarkerList = () => {
+    const [profiles, setProfiles] = useState([]);
+    useEffect(() => {
+        const getProfiles = async () => {
+            const response = await fetch("http://localhost:5002/profiles");
+            const data = await response.json();
+            setProfiles(data);
+        };
+        getProfiles();
+    }, []);
+
+    return profiles.map((profile) => {
+        return (
+            <>
+                <Marker
+                    key={profile.id}
+                    profile={profile}
+                    lat={profile.coordinates[0]}
+                    lng={profile.coordinates[1]}
+                />
+            </>
+        );
+    });
+};
 
 const Map = (props) => {
     const [center, setCenter] = useState({ lat: -40.9006, lng: 174.886 });
     const [zoom, setZoom] = useState(12);
-    const [profiles, setProfiles] = useState([]);
-
-    // useEffect(() => {
-    const getProfiles = async () => {
-        const response = await fetch("http://localhost:5002/profiles");
-        const data = await response.json();
-        setProfiles(data);
-    };
-    getProfiles();
-    // }, []);
+    // const [profiles, setProfiles] = useState([]);
 
     return (
         <div style={{ height: "100vh", width: "100%" }}>
@@ -27,18 +41,7 @@ const Map = (props) => {
                 defaultCenter={center}
                 defaultZoom={zoom}
             >
-                {profiles.map((profile) => {
-                    return (
-                        <>
-                            <Marker
-                                key={profile.id}
-                                profile={profile}
-                                lat={profile.coordinates[0]}
-                                lng={profile.coordinates[1]}
-                            />
-                        </>
-                    );
-                })}
+                <MarkerList />
             </GoogleMapReact>
         </div>
     );
