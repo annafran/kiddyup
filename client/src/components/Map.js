eced32ecd8dab4e3bfc6c9924cf4f054e4df3dc1
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-07-19 10:00:01 +1200

    move MarkerList as separate component

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,36 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
-import Marker from "./Marker";
-
-const MarkerList = () => {
-  const [profiles, setProfiles] = useState([]);
-  useEffect(() => {
-    const getProfiles = async () => {
-      const response = await fetch("http://localhost:5002/profiles");
-      const data = await response.json();
-      setProfiles(data);
-    };
-    getProfiles();
-  }, []);
-
-  return profiles.map((profile) => {
-    return (
-      <>
-        <Marker
-          key={profile.id}
-          profile={profile}
-          lat={profile.coordinates[0]}
-          lng={profile.coordinates[1]}
-        />
-      </>
-    );
-  });
-};
+import MarkerList from "./MarkerList";
 
 const Map = (props) => {
   const [center, setCenter] = useState({ lat: -40.9006, lng: 174.886 });
   const [zoom, setZoom] = useState(12);
-  // const [profiles, setProfiles] = useState([]);
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
