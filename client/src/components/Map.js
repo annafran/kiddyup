a142a873352a8e933deb0e57041690c085db734c
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-07-19 12:40:11 +1200

    fix rendering of markers on map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,21 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
-import MarkerList from "./MarkerList";
+import { Link } from "react-router-dom";
+import "./Marker.css";
 
-const Map = (props) => {
+const Marker = ({ image, alt, link }) => {
+  return (
+    <div>
+      <img
+        className="marker"
+        src={image}
+        alt={alt}
+        component={Link}
+        to={link}
+      />
+    </div>
+  );
+};
+
+const Map = () => {
   const [center, setCenter] = useState({ lat: -40.9006, lng: 174.886 });
-  const [zoom, setZoom] = useState(12);
+  const [zoom, setZoom] = useState(1);
+  const [profiles, setProfiles] = useState([]);
+  useEffect(() => {
+    const getProfiles = async () => {
+      const response = await fetch("http://localhost:5002/profiles");
+      const data = await response.json();
+      setProfiles(data);
+    };
+    getProfiles();
+  }, []);
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{
-          key: process.env.GOOGLE_MAPS_REACT_API_KEY,
+          key: process.env.REACT_APP_API_KEY,
         }}
         defaultCenter={center}
         defaultZoom={zoom}
       >
-        <MarkerList />
+        {profiles.map((profile) => {
+          return (
+            <Marker
+              lat={profile.coordinates[0]}
+              lng={profile.coordinates[1]}
+              image={profile.profilePhoto}
+              alt={profile.firstName}
+              link={`/profiles/${profile.id}`}
+            />
+          );
+        })}
       </GoogleMapReact>
     </div>
   );
--- client/src/components/Marker.js
+++ client/src/components/Marker.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { Avatar } from "@mantine/core";
+// import { Avatar } from "@mantine/core";
 import { Link } from "react-router-dom";
 import "./Marker.css";
 
 const Marker = ({ profile }) => {
-    return (
-        <div>
-            <Avatar
-                src={profile.profilePhoto}
-                alt={profile.firstName}
-                size="sm"
-                radius="lg"
-                component={Link}
-                to={`/profiles/${profile.id}`}
-            />
-        </div>
-    );
+  return (
+    <img
+      src={profile.profilePhoto}
+      alt={profile.firstName}
+      size="sm"
+      radius="lg"
+      component={Link}
+      to={`/profiles/${profile.id}`}
+    />
+  );
 };
 
 export default Marker;
