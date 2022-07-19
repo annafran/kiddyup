0ad134e290f5cb7ef0daae8044585dc14f2b7246
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-07-19 18:51:33 +1200

    update index.html meta data

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,23 +4,23 @@ import { Text } from "@mantine/core";
 import father from "../assets/father.svg";
 
 const Home = () => {
-  return (
-    <>
-      <Title order={3} align="center" mt="4rem" ml="2rem" mr="2rem">
-        Are you a mother or father who wants to meet other parents?
-      </Title>
-      <Image
-        width="30rem"
-        align="center"
-        src={father}
-        alt="Random unsplash image"
-        mt="4rem"
-      />
-      <Text size="xl" align="center" mt="4rem">
-        Search by your location and connect!
-      </Text>
-    </>
-  );
+    return (
+        <>
+            <Title order={3} align="center" mt="4rem" ml="2rem" mr="2rem">
+                Are you a mother or father who wants to meet other parents?
+            </Title>
+            <Image
+                width="20rem"
+                align="center"
+                src={father}
+                alt="Random unsplash image"
+                mt="4rem"
+            />
+            <Text size="xl" align="center" mt="4rem" mb="4rem">
+                Search by your location and connect!
+            </Text>
+        </>
+    );
 };
 
 export default Home;
