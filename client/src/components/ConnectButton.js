235b52b2b378da5daa1c60c26252d695c9d3bf28
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-07-21 11:32:03 +1200

    add mongodburi environment variable

--- client/src/components/ConnectButton.js
+++ client/src/components/ConnectButton.js
@@ -1,4 +1,3 @@
-// import { Link } from "react-router-dom";
 import { Button, createStyles } from "@mantine/core";
 import "./BackButton.css";
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,23 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config({
-    path: __dirname + "/../.env",
-    debug: true,
-    override: false,
+  path: __dirname + "/../.env",
+  debug: true,
+  override: false,
 });
+
+const mongoDbUri = process.env.MONGO_URI || "mongodb://localhost:27017/kiddyup";
 
 const port = process.env.PORT || 5001;
 mongoose
-    .connect("mongodb://localhost:27017/kiddyup")
-    .then(() => {
-        console.log("Connected to mongoose");
-    })
-    .catch((error) => {
-        console.log("Connection to mongoose failed", error);
-    });
+  .connect(mongoDbUri)
+  .then(() => {
+    console.log("Connected to mongoose");
+  })
+  .catch((error) => {
+    console.log("Connection to mongoose failed", error);
+  });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
