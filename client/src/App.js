4feada64651495e6a96067f23c54dc396055c9ae
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-07-20 12:27:14 +1200

    remove fonts from index.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import ProfileList from "./components/ProfileList";
 import AddProfile from "./components/AddProfile";
 import Layout from "./components/Layout";
@@ -8,19 +7,19 @@ import ProfilePage from "./components/ProfilePage";
 import Map from "./components/Map";
 
 function App() {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="profiles" element={<ProfileList />} />
-                    <Route path="map" element={<Map />} />
-                    <Route path="signup" element={<AddProfile />} />
-                    <Route path="profiles/:id" element={<ProfilePage />} />
-                </Route>
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="profiles" element={<ProfileList />} />
+          <Route path="map" element={<Map />} />
+          <Route path="signup" element={<AddProfile />} />
+          <Route path="profiles/:id" element={<ProfilePage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
