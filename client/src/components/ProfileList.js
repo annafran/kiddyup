746d04df4e15b7764f405ac6b6e19940a8faeb74
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-07-26 09:40:12 +1200

    add loader and text if no profiles in database

--- client/src/components/ProfileList.js
+++ client/src/components/ProfileList.js
@@ -1,22 +1,43 @@
 import { useEffect, useState } from "react";
-import { Container, Grid } from "@mantine/core";
+import { Loader } from "@mantine/core";
+import { Container, Grid, Text } from "@mantine/core";
 import Profile from "./Profile";
 
 const ProfileList = () => {
   const [profiles, setProfiles] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const getProfiles = async () => {
       const response = await fetch(`${process.env.REACT_APP_API_URL}/profiles`);
       const data = await response.json();
       setProfiles(data);
+      setLoading(false);
     };
     getProfiles();
   }, []);
 
-  return (
-    <>
-      <Container size="xl">
+  const renderProfiles = () => {
+    if (loading) {
+      return (
+        <>
+          <Grid justify="center" mt="5rem">
+            <Loader size={100} color="cyan" />
+          </Grid>
+        </>
+      );
+    }
+
+    if (profiles.length === 0) {
+      return (
+        <Grid justify="center" mt="5rem">
+          <Text>Oops no profiles found!</Text>
+        </Grid>
+      );
+    }
+    return (
+      <>
         <Grid justify="center" gutter={0} mt="2rem">
           {profiles.map((profile) => {
             return (
@@ -28,7 +49,13 @@ const ProfileList = () => {
             );
           })}
         </Grid>
-      </Container>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <Container size="xl">{renderProfiles()}</Container>
     </>
   );
 };
