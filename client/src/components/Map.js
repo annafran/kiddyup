659c1f6c6fc368b5136ce752ca5a2ed190b44404
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-08-11 16:23:09 +1200

    add mapbox and leaflet to replace google api map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
-import GoogleMapReact from "google-map-react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import Marker from "./Marker";
+import "leaflet/dist/leaflet.css";
+
+const accessToken = process.env.REACT_APP_MAPBOX_KEY;
 
 const Map = ({ center, setActive }) => {
   const [zoom] = useState(5);
@@ -15,8 +18,30 @@ const Map = ({ center, setActive }) => {
   }, []);
 
   return (
-    <div style={{ height: "90vh", width: "100%" }}>
-      <GoogleMapReact
+    <div>
+      <MapContainer
+        center={center}
+        zoom={5}
+        scrollWheelZoom={false}
+        style={{ height: "90vh", width: "100%" }}
+      >
+        <TileLayer
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+          url={`https://api.mapbox.com/styles/v1/annafran1/cl6oig57w003014odi62akas5/tiles/256/{z}/{x}/{y}@2x?access_token=${accessToken}`}
+        />
+        {profiles.map((profile) => {
+          return (
+            <Marker
+              position={[profile.coordinates[0], profile.coordinates[1]]}
+              image={profile.profilePhoto}
+              alt={profile.firstName}
+              link={`/profiles/${profile.id}`}
+              setActive={setActive}
+            />
+          );
+        })}
+      </MapContainer>
+      {/* <GoogleMapReact
         bootstrapURLKeys={{
           key: process.env.REACT_APP_API_KEY,
         }}
@@ -35,7 +60,7 @@ const Map = ({ center, setActive }) => {
             />
           );
         })}
-      </GoogleMapReact>
+      </GoogleMapReact> */}
     </div>
   );
 };
